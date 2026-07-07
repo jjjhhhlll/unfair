61baa98ca11beb90f869ef6d1ee137a3840b3710
Jon Landis <jjjhhhlll@gmail.com>
2026-07-07 17:32:20 -0400

    fixed bug where shader colored archville blue and plasma projectiles black

--- shaders/colorize.glsl
+++ shaders/colorize.glsl
@@ -7,25 +7,25 @@
 // float uContrast 
 
 void main() {
-    vec4 color = texture(InputTexture, TexCoord);
-    vec3 col = color.rgb;
+    vec4 base = texture(InputTexture, TexCoord);
+    vec3 lumCoeff = vec3(0.299, 0.587, 0.114);
+    float lum = dot(lumCoeff, base.rgb);
+    vec3 blend = lum.rrr;
 
-    // bleach bypass style effect that raises contrast and blends with original colors
-    vec3 lum = vec3(0.299, 0.587, 0.114);
-    float bw = dot(col, lum);
-    vec3 bwVec = vec3(bw);
+    float L = min(1, max(0, uIntensity*(lum - uContrast)));
 
-    float contrast = min(1.0, max(0.0, uIntensity * (bw - uContrast)));
+    vec3 result1 = 2.0 * base.rgb * blend;
+    // need clamp because blend can get too large causing white to change to blue/black
+    // archville, plasma, bfg, etc...
+    vec3 result2 =  1.0 - (2.0 * (1.0 - clamp(blend, 0.0, 0.799)) * (1.0 - base.rgb));
+    vec3 newColor = mix(result1, result2, L);
 
-    vec3 result1 = 2.0 * color.rgb * bwVec;
-    vec3 result2 = 1.0 - 2.0 * (1.0 - bwVec) * (1.0 - color.rgb);
-    col = mix(result1, result2, contrast);
+    float A2 = uOpacity * base.a;
+    vec3 mixRgb = A2 * newColor.rgb;
+    mixRgb += ((1.0 - A2) * base.rgb);
 
-    float opacity = uOpacity * color.a;
-    col = opacity * col.rgb + ((1.0 - opacity) * color.rgb);
+    // add pulsating effect
+    mixRgb *= 1.25 + 0.17 * sin(0.88 * timer);
 
-    // flicker slowly (subtle light level change)
-    col *= 1.25 + 0.15 * sin(0.8 * timer);
-
-    FragColor = vec4(col, color.a);
+    FragColor = vec4(mixRgb, base.a);
 }
